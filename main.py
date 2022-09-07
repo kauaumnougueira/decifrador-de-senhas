877ea3d5b6f3e9f78af62b78a5ba85cb166db956
kauaumonougueira <kaua_kna@hotmail.com>
2022-09-07 17:50:58 -0300

    start

---
+++ main.py
@@ -0,0 +1,7 @@
+senha = ""
+print("-----------------------------------")
+print("-Digite sua senha de 8 caracteres: ")
+print("-----------------------------------")
+input(senha)
+
+print(senha)
---
+++ neuronio/decifrador.py
@@ -0,0 +1,8 @@
+#caracteres
+alfabeto = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%&?"
+list(alfabeto)
+
+def decifrar():
+    for i in range(8):
+        print(i)
+
